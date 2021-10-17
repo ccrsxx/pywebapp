f46c76eb098568b37c0c49cec9533856548b72b5
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-10-17 21:57:08 +0700

    fix caching

--- games/guess_word.py
+++ games/guess_word.py
@@ -14,7 +14,6 @@ def get_word(language: str, length: int) -> str:
 
         return word
 
-@st.cache
 def restart(language: str = 'English', length: int = 6):
     st.session_state.word = get_word(language, length)
     st.session_state.guessed = []
