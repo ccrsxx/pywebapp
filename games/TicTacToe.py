b1134862a2a8bdc6c8cc3aff5c1c7baf4d7bfa99
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-10-19 22:04:33 +0700

    almost done

--- games/TicTacToe.py
+++ games/TicTacToe.py
@@ -1,14 +1,36 @@
+from numpy.core.numeric import ones_like
 import streamlit as st
 import numpy as np
+import random
 
-def checkRows(board):
+
+def init(post_init=False):
+    if not post_init:
+        st.session_state.opponent = 'Computer'
+        st.session_state.win = {'X': 0, 'O': 0}
+    st.session_state.board = np.full((3, 3), '.', dtype=str)
+    st.session_state.player = 'X'
+    st.session_state.mutate = True
+    st.session_state.winner = None
+
+def check_state():
+    if st.session_state.winner and st.session_state.mutate:
+        st.session_state.mutate = False
+        st.session_state.win[st.session_state.winner] =\
+             st.session_state.win.get(st.session_state.winner, 0) + 1
+        st.success(f"Congrats! {st.session_state.winner} won the game! 🎈")
+    elif not board_available() and not st.session_state.winner:
+        st.info(f'Tie')
+
+
+def check_rows(board):
     for row in board:
         if len(set(row)) == 1:
             return row[0]
     return None
 
 
-def checkDiagonals(board):
+def check_diagonals(board):
     if len(set([board[i][i] for i in range(len(board))])) == 1:
         return board[0][0]
     if len(set([board[i][len(board) - i - 1] for i in range(len(board))])) == 1:
@@ -16,25 +38,31 @@ def checkDiagonals(board):
     return None
 
 
-def checkWin(board):
+def check_win(board):
     # transposition to check rows, then columns
-    for newBoard in [board, np.transpose(board)]:
-        result = checkRows(newBoard)
+    for new_board in [board, np.transpose(board)]:
+        result = check_rows(new_board)
         if result:
             return result
-    return checkDiagonals(board)
+    return check_diagonals(board)
 
 
-def init():
-    st.session_state.board = np.full((3, 3), '.', dtype=str)
-    st.session_state.next_player = 'X'
-    st.session_state.winner = None
+def board_available(extra=False):
+    raw_moves = [row for col in st.session_state.board.tolist() for row in col]
+    available_moves = [i for i, spot in enumerate(raw_moves) if spot == '.']
+    if extra:
+        return [(i // 3, i % 3) for i in available_moves]
+    return available_moves
+
 
 def computer_player():
-    pass
-
+    moves = board_available(True)
+    if moves:
+        c_move = random.choice(moves)
+        handle_click(c_move[0], c_move[1])
 
 def play():
+    global handle_click
     st.write("""
         # 🕹️ Tic Tac Toe
         in development now for university... by **ccrsxx#8408**
@@ -44,16 +72,25 @@ def play():
     if "board" not in st.session_state:
         init()
 
+    reset, won_game, player, settings = st.columns([.5, .6,  1, 1])
+    reset.button('New game', on_click=init, args=(True, ))
+
+    with settings.expander('Settings'):
+        st.write('**Warning**: changing one of these settings will restart your game')
+        st.selectbox('Set opponent', ['Computer', 'Human'], key='opponent', on_change=init, args=(True, ))
+
     # Define callbacks to handle button clicks.
     def handle_click(i, j):
-        st.write(i, j)
-        if not st.session_state.winner:
+        if (i, j) not in board_available(extra=True):
+            pass
+        elif not st.session_state.winner:
             # TODO: Handle the case when nobody wins but the game is over!
-            st.session_state.board[i, j] = st.session_state.next_player
-            st.session_state.next_player = (
-                "O" if st.session_state.next_player == "X" else "X"
+            st.session_state.board[i, j] = st.session_state.player
+            # switch the player after the updating the board
+            st.session_state.player = (
+                "O" if st.session_state.player == "X" else "X"
             )
-            winner = checkWin(st.session_state.board)
+            winner = check_win(st.session_state.board)
             if winner != ".":
                 st.session_state.winner = winner
 
@@ -64,14 +101,17 @@ def play():
             cols[j].button(
                 field,
                 key=f"{i}-{j}",
-                on_click=handle_click,
+                on_click=handle_click if st.session_state.player == 'X' or st.session_state.opponent == 'Human' else computer_player(),
                 args=(i, j),
             )
 
-    if st.session_state.winner:
-        st.success(f"Congrats! {st.session_state.winner} won the game! 🎈")
+    check_state()
+
+    won_game.button(f'❌{st.session_state.win["X"]} 🆚 {st.session_state.win["O"]}⭕')
+    player.button(f'{"❌" if st.session_state.player == "X" else "⭕"}\'s turn' if not st.session_state.winner else f'🏁 Game finished')
 
     st.write(st.session_state)
+
 
 if __name__ == '__main__':
     play()
